e0a6548cd2eaa22ffed9c138c79de1f599bc90fd
mombasic <mahir.ombasic@gmail.com>
2026-04-20 13:58:51 +0200

    u1

--- tileset.tsx
+++ tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.1" name="tileset" tilewidth="16" tileheight="16" tilecount="294" columns="21">
  <grid orientation="orthogonal" width="32" height="32"/>
- <image source="../gothicvania church files/gothicvania church files/Assets/ENVIRONMENT/tileset.png" width="336" height="224"/>
+ <image source="gothicvania church files/gothicvania church files/Assets/ENVIRONMENT/tileset.png" width="336" height="224"/>
 </tileset>
